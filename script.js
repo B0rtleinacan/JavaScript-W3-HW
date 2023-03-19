a91f05d125d26e19479b83bcc26b2b7c14b9f7ff
B0rtleinacan <Cloverlyn123@gmail.com>
2023-03-19 11:33:54 -0500

    Added code to the JS file, set variables for special characters, numbers, lower cased and upper cased characters and began creating a function to fun through if things have the things needed to create a randomly generated password.

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// Array for special characters for Users to have more security in their generated password
+var characters = [ '!', '"', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', '=', '-', '_', '+', '<', '>', '?', '\\', '|', ';', ':', ' ', '[', ']', '{', '}']
+  
+// Array of #'s to use in genereated password
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+// Array of lower case letters for the User to use in generated password
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+// Array of uppder case letters for the User to use in generated password
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+passwordOptions = () => {
+  let length = parseInt(
+    alert('How many charcters do you want your password to be?')
+  )
+
+  if (Number.isNaN(length)) {
+    alert('The password must have a length provided')
+  }
+
+  if (length < 8) {
+    alert('The password must be 8 or more characters')
+  }
+
+  if (length > 128) {
+    alert('The password must be at least or less than 128 characters')
+  }
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
