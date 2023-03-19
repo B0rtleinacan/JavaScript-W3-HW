df644084a036d90dfe00a528c6845ee99fb137eb
B0rtleinacan <Cloverlyn123@gmail.com>
2023-03-19 17:25:23 -0500

    Added confirms to the main function

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 // Array for special characters for Users to have more security in their generated password
-var characters = [ '!', '"', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', '=', '-', '_', '+', '<', '>', '?', '\\', '|', ';', ':', ' ', '[', ']', '{', '}']
+var characters = [ '!', '"', '@', '#', '$', '%', '^', '&', '*', '(', ')', '.', '=', '-', '_', '+', '<', '>', '?', '\\', '|', ';', ':', ' ', '[', ']', '{', '}'];
   
 // Array of #'s to use in genereated password
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
 // Array of lower case letters for the User to use in generated password
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Array of uppder case letters for the User to use in generated password
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 passwordOptions = () => {
   let length = parseInt(
     alert('How many charcters do you want your password to be?')
-  )
+  );
 
   if (Number.isNaN(length)) {
-    alert('The password must have a length provided')
+    alert('The password must have a length provided');
   }
 
   if (length < 8) {
-    alert('The password must be 8 or more characters')
+    alert('The password must be 8 or more characters');
   }
 
   if (length > 128) {
-    alert('The password must be at least or less than 128 characters')
+    alert('The password must be at least or less than 128 characters');
   }
+
+  var hasCharacters = confirm('This password has special characters.');
+
+  var hasNumbers = confirm('This password has numbers.');
+
+  var hasLowercase = confirm('This password has lower case.');
+
+  var hasUppercase = confirm('This password has upper case.');
 }
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
